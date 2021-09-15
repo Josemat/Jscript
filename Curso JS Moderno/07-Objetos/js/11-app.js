ed7c83eaa37e5672dbc2924bbe6fdf46226e912d
Emmanuel <emmatulian@gmail.com>
2021-09-15 17:17:17 -0300

    finalizacion capitulos Objects

--- Curso JS Moderno/07-Objetos/js/11-app.js
+++ Curso JS Moderno/07-Objetos/js/11-app.js
@@ -1,4 +1,4 @@
-const producto = {
+/*const producto = {
     nombre: "Monitor 20 Pulgadas", 
     precio: 300,
     disponible: true,
@@ -6,6 +6,14 @@ const producto = {
         console.log(`El producto: ${this.nombre} tiene un precio de ${this.precio}`)//THIS, se refiere a que actua en el mismo objeto
     }
 }
-
 producto.mostrarInfo();
-
+*/
+const producto = {
+    nombre:"Monitor",
+    disponibilidad:true,
+    precio:300,
+    resultado:function(){
+        console.log(`El precio del producto:${this.nombre} es de ${this.precio}`) //el this. hace referencia al alcance del scope. Es decir, busca UNICAMENTE en el objeto.
+    }
+}
+producto.resultado();
