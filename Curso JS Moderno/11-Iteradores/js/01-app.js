527a8c573bca8ec75ce76f25dc73974214c05091
Emmanuel <emmatulian@gmail.com>
2021-10-07 17:37:35 -0300

    Un poquito
    For loop

---
+++ Curso JS Moderno/11-Iteradores/js/01-app.js
@@ -0,0 +1,10 @@
+for(let i=1; i<=10; i++){
+    if(i%2 ===0){
+        console.log(`${i} es PAR`)
+    }else{
+        console.log(`${i} es IMPAR`)
+    }
+    if(i%4===0){
+        console.log(`${i} es modulo de 4`)
+    }
+}
