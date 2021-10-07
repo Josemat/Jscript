9d5209a46f927f394c0bc029d707a10b41fce0a7
Emmanuel <emmatulian@gmail.com>
2021-10-06 22:26:45 -0300

    123

--- Curso JS Moderno/09-Funciones/js/03-app.js
+++ Curso JS Moderno/09-Funciones/js/03-app.js
@@ -1,9 +1,1 @@
-/*Libreria de funciones nativas
- tecleando "window" en la consola te aparece un listado de funciones que vienen 'predefinidas'
-por ejemplo:
-
-alert('Hola mundo');
-
-prompt('Cual es tu edad?')
-*/
-console.log(parseInt('20'));//Pasa un string a entero
+//Libreria de funciones nativas
