e8c0df5f8fef204b9ff4a8b95520321655f0163a
Emmanuel <emmatulian@gmail.com>
2021-10-12 23:20:15 -0300

    while-do_while

---
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -0,0 +1,7 @@
+// Do while
+//se ejecuta al menos una vez, ya que la condicion se evalua al final
+let i=10;
+do{//Haz esto
+console.log(`El numero es: ${i}!`)
+    i++
+}while(i<10);//mientras se cumpla esta condicion
