c659913798232d9f6baed86ace1f0de53e1d9948
Emmanuel <emmatulian@gmail.com>
2021-10-15 16:37:04 -0300

    .some

--- Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -8,3 +8,30 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+
+//comprobar si un valor existe en un array
+
+meses.forEach((mes)=>{return console.log(mes)})
+meses.forEach(mes=>console.log(mes))//El codigo al ser de una linea puede quedar asi
+meses.forEach(mes=>{
+    if(mes === "Junio"){
+        console.log(`Si existe ${mes}`)
+    }
+})
+//Otra forma con el metodo .includes (devuelve boolean)
+
+const comprobar = meses.includes("Febrero")
+console.log(comprobar)
+
+const comprobar2 = meses.includes("Diciembre")
+console.log(comprobar2)
+
+
+//Para consultar datos en un array de objetos (boolean)
+const existe = carrito.some(producto=>{return producto.nombre === "Tablet"})
+console.log(`Existe: ${existe}`)
+
+
+const existe2 = carrito.some(producto=>producto.precio === 300) // El return esta implicito)
+console.log(existe2)
