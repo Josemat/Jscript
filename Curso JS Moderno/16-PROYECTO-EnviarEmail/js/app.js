ec649a5a19d587aa07d6c741b7f48756a4b28c16
Emmanuel <emmatulian@gmail.com>
2021-11-09 16:29:44 -0300

    mensaje error

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -5,6 +5,8 @@ const asunto = document.querySelector('#asunto')
 const mensaje = document.querySelector('#mensaje')
 const formulario = document.querySelector('#enviar-mail')
 
+
+
 eventListeners()
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', iniciarApp)
@@ -20,25 +22,49 @@ function iniciarApp(){
 }
 
 function validarFormulario(e){
-    if(e.target.value === ''){
-        e.target.classList.add('border','border-red-500')
-        mensajeError()
-    }else{
-        e.target.classList.add('border','border-green-500')
-        
-  
-    }
 
+    
+    if(e.target.value.length > 0){
+
+        const errors = document.querySelector('p.error')
+        if (errors)
+        {errors.remove()}
+
+        e.target.classList.remove('border','border-red-500')
+        e.target.classList.add('border','border-green-500')
+    }else{
+        e.target.classList.remove('border','border-green-500')
+        e.target.classList.add('border','border-red-500')
+        mensajeError('Todos los campos son obligatorios')
+
+        }
+    if(e.target.type === 'email'){
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        
+        // console.log(re.test(e.target.value), 'probando expresion regular')
+        if(re.test(e.target.value)){
+            const errors = document.querySelector('.error')
+            if (errors)
+            {errors.remove()}
+            e.target.classList.remove('border','border-red-500')
+            e.target.classList.add('border','border-green-500')
+        }else{
+            e.target.classList.remove('border','border-green-500')
+            e.target.classList.add('border','border-red-500')
+            mensajeError('Email no válido')
+            
+        }
+    }    
 }
-function mensajeError(){
+function mensajeError(mensaje){
     const errors = document.createElement('p')
-    errors.textContent= 'Todos los campos son obligatorios'
+    errors.textContent= mensaje
     errors.classList.add('error', 'text-red-500','border','border-red-500', 'mt-5', 'p-3', 'bg-red-100', 'text-center')
 
     //Agrega una sola vez el error
     const hayError = document.querySelectorAll('.error')
     if(hayError.length === 0){
         formulario.appendChild(errors)
-        console.log('asd')
+        
     }
 }
