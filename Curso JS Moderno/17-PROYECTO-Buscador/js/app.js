a720eaad3b74b6b641dd807abc8a4fc9e49c332a
Emmanuel <emmatulian@gmail.com>
2021-11-15 16:34:50 -0300

    marca

--- Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,38 +1,70 @@
 //Variables
-const resultado = document.querySelector('#resultado')
 const marca = document.querySelector('#marca')
-const modelo = document.querySelector('#modelo')
 const year = document.querySelector('#year')
-const precio = document.querySelector('#precio')
-const puertas = document.querySelector('#puertas')
-const transmision = document.querySelector('#transmision')
 const minimo = document.querySelector('#minimo')
 const maximo = document.querySelector('#maximo')
+const puertas = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
 const color = document.querySelector('#color')
+
+//Contenedor para la busqueda
+const resultado = document.querySelector('#resultado')
+
 
 const max = new Date().getFullYear()
 const min = max - 10
 
 const datosBusqueda = {
     marca:'',
-    modelo:'',
     year:'',
+    minimo:'',
+    maximo:'',
     puertas:'',
     transmision:'',
     color:'',
-    minimo:'',
-    maximo:'',
-}
+    }
 //Eventlisteners
 
 document.addEventListener('DOMContentLoaded',()=>{
     mostrarAutos()
     //Llenar el year
     llenarSelect()
+    
 })
+
+marca.addEventListener('change', e=>{
+    datosBusqueda.marca = e.target.value
+    filtrarAuto()
+})
+year.addEventListener('change', e=>{
+    datosBusqueda.year = parseInt(e.target.value)//hay que pasarlos a enteros xq si no te da un array vacio
+    
+    filtrarAuto()
+})
+minimo.addEventListener('change', e=>{
+    datosBusqueda.minimo = e.target.value
+})
+maximo.addEventListener('change', e=>{
+    datosBusqueda.maximo = e.target.value
+})
+puertas.addEventListener('change', e=>{
+    datosBusqueda.puertas = e.target.value
+})
+transmision.addEventListener('change', e=>{
+    datosBusqueda.transmision = e.target.value
+})
+color.addEventListener('change', e=>{
+    datosBusqueda.color = e.target.value
+    
+})
+
+
 
 //Funciones
 function mostrarAutos(){
+
+    limpiarHTML
+
     autos.forEach(auto => {
         const  {marca, modelo, year, precio, puertas, color, transmision}= auto
         const vehiculo = document.createElement('p')
@@ -52,3 +84,41 @@ function llenarSelect(){
         
         }
     }
+//Funcion que filtra en base a la busqueda
+
+    function filtrarAuto(){
+        const resultado = autos.filter( filtrarMarca ).filter( filtrarYear )
+        console.log(resultado)
+
+    }
+
+    function filtrarMarca(auto){
+        if(datosBusqueda.marca){
+            return auto.marca === datosBusqueda.marca // retorna los valores del array que le pasamos que son iguales a los datos de busqueda ingresado
+        }
+        return auto
+    }
+    
+        function filtrarYear(auto){
+            // console.log(typeof datosBusqueda.year) //String
+            // console.log(typeof auto.year) //numero
+            if(datosBusqueda.year){
+            return auto.year === datosBusqueda.year 
+        }
+        return auto
+    }
+
+
+
+
+
+
+
+
+
+
+function limpiarHTML(){
+    while( resultado.firstElementChild){
+        resultado.removeChild(resultado.firstElementChild)
+    }
+}
