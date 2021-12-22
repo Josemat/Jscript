3b2234e972df085f502c03fe4cd82630a63629c4
Emmanuel <emmatulian@gmail.com>
2021-12-22 17:06:03 -0300

    protoPT2

--- Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,9 +1,21 @@
 //Constructores
 
-function seguro(marca, year, tipo){
+function Seguro(marca, year, tipo){
     this.marca = marca;
     this.year = year;
     this.tipo = tipo;
+}
+
+//Realiza la cotizacion con los datos
+Seguro.prototype.cotizarSeguro = function(){
+/*
+americano = 1.15
+asiatico  = 1.05
+europeo = 1.35
+*/
+console.log(this.tipo)
+console.log(this.marca)
+console.log(this.year)
 }
 
 function UI(){};
@@ -21,11 +33,56 @@ UI.prototype.llenarOpciones = ()=>{
         }    
 }
 
+//Muestra alertas en pantalla
+UI.prototype.mostrarAlerta = (mensaje, tipo)=>{
+    
+    const div = document.createElement('div');
+    div.textContent= "";
+    
+    if(tipo === "error"){  
+        div.classList.add(tipo, "mt-10");
+
+    }else{
+        div.classList.add(tipo, "mt-10")
+        
+    }
+    div.textContent = mensaje
+    const form = document.querySelector("#cotizar-seguro");
+    form.insertBefore(div, document.querySelector("#resultado"));
+    setTimeout(()=>{
+        div.remove();
+    },2000)
+}
+
 //Instanciar UI
 const ui = new UI();    
 
 
 document.addEventListener("DOMContentLoaded",()=>{
     ui.llenarOpciones()
+    
 
-})
+    })
+    validarFormulario()
+
+function validarFormulario(){
+    
+    const submit = document.querySelector('#cotizar-seguro')
+    submit.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        const valorMarca = document.querySelector('#marca').value
+        const valorYear = document.querySelector('#year').value
+        const valorTipo = document.querySelector('input[name="tipo"]:checked').value
+        if(valorMarca === '' || valorYear === '' || valorTipo ===''){
+                ui.mostrarAlerta("Todos los campos son obligatorios...", "error");
+                return //Esto genera que se detenga la ejecucion
+        }else{
+            ui.mostrarAlerta("Validando...", "correcto");
+        }
+//Instanciar el seguro
+      const seguro = new Seguro(valorMarca, valorYear, valorTipo)
+      seguro.cotizarSeguro()
+//
+
+      
+    })}
