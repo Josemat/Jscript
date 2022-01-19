58dfa8b7e34cc4cfc7630058dd4766135c1cb2a2
Emmanuel <emmatulian@gmail.com>
2022-01-19 14:30:41 -0300

    2

--- Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -1,9 +1,58 @@
 (function(){
 
-    // const botonEditar
+    document.addEventListener('DOMContentLoaded',()=>{
+        const cliente = window.location.search.slice(4)
+        
+        /*
+        // Otra forma mas pro de buscar los parametros pasados por una url es:
+        const parametrosURL = new URLSearchParams(window.location.search)
+        const idCliente = parametrosURL.get('id')
+        console.log(idCliente)
+        */
 
-    console.log(window.location.search.slice(4))
+        // DEBEMOS validar si hay busqueda de id 
+        if(cliente){
+            
+        editarCliente(cliente)
+        }else
+        console.log('No hay nada pa mostrar')
+    })
 
-    function editarCliente(){}
+    
+    
+    let DB;
+    function editarCliente(cliente){
+        
+        //abrir la conexion
+        const abrirConexion = window.indexedDB.open('crm', 1)
+        
+        abrirConexion.onerror= function(){
+            console.error('Hubo un error mostrando los clientes')
+        }
+        abrirConexion.onsuccess = function(){
+            DB = abrirConexion.result
+            //Obtenemos los datos de la DB
+            console.log('Bien')
+            const objectStore = DB.transaction('crm').objectStore('crm')
+            objectStore.openCursor().onsuccess = function(e){
+                const cursor = e.target.result
+                if(cursor){
+                    console.log(cursor.value.id)
+                    const cliente1 = cursor.value.id
+                    const cliente2 = Number(cliente)
+                    
+                    if(cliente1 === cliente2){
+                        const {nombre, email, telefono, empresa } = cursor.value
+                        document.querySelector('#nombre').value = nombre
+                        document.querySelector('#email').value = email
+                        document.querySelector('#telefono').value = telefono
+                        document.querySelector('#empresa').value = empresa
+                    }
+                    cursor.continue()
+                }
+                }
+            }
+        }
+        
 
 })();
