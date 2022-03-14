15409f2c02c9bc43365c38c22afda75dc66c9e8f
Emmanuel <emmatulian@gmail.com>
2022-03-14 01:02:18 -0300

    15min

---
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,48 @@
+let cliente = {
+    mesa:'',
+    hora:'',
+    pedido:[]
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente')
+btnGuardarCliente.addEventListener('click', guardarCliente)
+
+function guardarCliente(){
+    const mesa = document.querySelector('#mesa').value
+    const hora = document.querySelector('#hora').value
+    /*
+    if(!mesa || !hora){
+        console.log('Hay al menos un campo vacio')
+    }else{
+        console.log('Todos los campos estan llenos')
+    }
+    */
+//    Otra forma de validar los datos es:
+
+    const camposVacios = [mesa, hora].some(element => element === '')
+    if(camposVacios){
+        const existeAlerta = document.querySelector('.invalid-feedback')
+        if(!existeAlerta){
+            const alerta = document.createElement('div')
+            alerta.classList.add('invalid-feedback','d-block','text-center')
+            alerta.textContent = 'Todos los campos son obligatorios'
+    
+            document.querySelector('.modal-body form').appendChild(alerta)
+            setTimeout(() => {
+                alerta.remove()
+            }, 2000);
+        }
+        return
+
+    }
+    // Asignar datos del formulario a cliente
+    cliente = {...cliente, mesa, hora} //Va en éste orden para que primero haga una copia del objeto y luego le agregue los valores de mesa y hora. Si primero le agregamos los valores de mesa y hora y luego la copia se pierden los valores asignados al comienzo ya que luego copia con un objeto vacio.
+    console.log(cliente)
+
+    
+//  Ocultar modal
+    const modalFormulario   =   document.querySelector('#formulario')
+    const modalBootstrap    =   bootstrap.Modal.getInstance(modalFormulario)
+    modalBootstrap.hide()
+    
+}
