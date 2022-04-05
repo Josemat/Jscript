343c60b4cd4c82afa89353059802c1c80ce7d92e
Emmanuel <emmatulian@gmail.com>
2022-04-05 09:58:29 -0300

    inicio React

---
+++ Curso JS Moderno/53.2 React App/prestamos/src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import Header from "./componentes/Header";
+
+
+function App() {
+  return (
+      <Fragment>
+        <Header
+        titulo="Nueva titulo probando"
+        />
+      </Fragment>
+        
+      
+  );
+}
+
+export default App;
---
+++ Curso JS Moderno/53.2 React App/prestamos/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header (props){
+
+    console.log(props)
+    return(
+
+        <h1>Hola mundo!!</h1>
+    )
+    
+}
+export default Header;
